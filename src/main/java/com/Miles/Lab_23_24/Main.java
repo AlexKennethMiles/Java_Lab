ee0e62eef59bc3afc6657928e0c868fe9657a83a
AlexKennethMiles <apuchkov17@gmail.com>
2020-11-29 20:54:38 +0300

    Add Lab_23_24

---
+++ src/main/java/com/Miles/Lab_23_24/Main.java
@@ -0,0 +1,10 @@
+package com.Miles.Lab_23_24;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Worker worker = new Worker();
+        worker.analyze();
+    }
+}
